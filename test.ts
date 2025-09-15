bdaf9fc711b0189371d9fc0819916b5231a0ea3b
person4202 <l31raya@allsaints.wa.edu.au>
2025-09-15 20:08:26 +0800

    Create test.ts

---
+++ test.ts
@@ -0,0 +1,7 @@
+extraButtons.onXButton(ControllerButtonEvent.Pressed, function () {
+    game.splash("X button pressed!")
+})
+
+extraButtons.onYButton(ControllerButtonEvent.Pressed, function () {
+    game.splash("Y button pressed!")
+})
